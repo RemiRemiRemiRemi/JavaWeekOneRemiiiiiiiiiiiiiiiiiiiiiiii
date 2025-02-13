222cc3a163ab1baef6775ddf85bae9c79b1791b5
Remi <zestysourcitrus@gmail.com>
2025-02-13 10:17:01 -0500

    d

---
+++ ClassTRemi.java
@@ -0,0 +1,20 @@
+/************
+*Project: Class T
+*Programmer: Remi-Laurent Guy
+*Date: Feb. 13th 2025
+*Program Name: ClassTRemi.Java
+*************/
+
+public class ClassTRemi{
+    public static void main(String[] args){
+
+
+System.out.println("\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t*\t*\t*\t*");
+System.out.println("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t \t \t*");
+
+System.out.println("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t \t \t*");
+
+System.out.println("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t \t \t*");
+
+}
+}
